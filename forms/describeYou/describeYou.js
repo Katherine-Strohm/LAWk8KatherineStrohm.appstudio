5f6e6300fcb9afa1a603f8ecf6b4c3d657d61b17
Katherine-Strohm <90561982+Katherine-Strohm@users.noreply.github.com>
2021-10-27 14:53:52 -0500

    final code

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,13 @@
-
-
-
-
-
+rdoType.onchange=function(){
+    if (rdoType.value == 0)   // picked the first choice
+      lblMessage.value = (`I would agree that you are a optimistic person too!`)
+    else if (rdoType.value == 1)
+        lblMessage.value = (`I would agree that you are a pesimistic person too!`)
+      else if (rdoType.value == 2)
+        lblMessage.value = (`I would agree that you are a trusting person too!`)
+    else if (rdoType.value == 3)
+        lblMessage.value = (`I would agree that you are a envious person too!`)
+}
+btnNext.onclick=function(){
+  ChangeForm(favExercises)
+}
--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,10 +1,28 @@
 let exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 let coreExercises = ["situps","plank"]
+let selections = []
 
 favExercises.onshow=function(){
+ selExercises.clear()
+    for (i = 0; i < exercises.length; i++) 
+        selExercises.addItem(exercises[i])
+  for (i = 0; i < coreExercises.length; i++)
+    selExercises.addItem(coreExercises[i])
+}
 
+selExercises.onchange=function(){
+  selections.push(selExercises.value)
+}
+
+btnSubmit.onclick=function(){
+  if (selections[0] == "situps" && selections[1] == "plank") {
+    lblExercise.value = (`You chose ${selections[0]} and ${selections[1]} - those are the two core exercises.`)
+  } else{
+    lblExercise.value = ("You did not pick the two core exercises.")
+}
 }
 
 
-
-
+btnNxt.onclick=function(){
+  ChangeForm(mobileNav)
+}
